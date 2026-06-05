8b7f2557e46cee919c13619151e5e92a7cf9fe46
Said Quioto <said.quioto@uap.edu.ar>
2026-06-05 14:54:25 -0300

    feat(auth): implementar feature toggle para alternar entre mock y fetch real a Spring Boot

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,34 @@
+export const loginService = async (email, password) => {
+    // Leemos el .env (por defecto viene como string)
+    const useMock = process.env.USE_MOCK === 'true';
 
-export const loginMockService = async (email, password) => {
-    // Simulamos un delay de red de 800ms para imitar la llamada a Spring Boot
-    await new Promise(resolve => setTimeout(resolve, 800));
+    // EL SIMULADOR (Mock) 
+    if (useMock) {
+        console.log("🟡 ALERTA: Usando Backend Simulado (Mock)");
+        await new Promise(resolve => setTimeout(resolve, 800)); // Delay simulado
 
-    // Lógica temporal de validación
-    if (email === "admin@waper.app" && password === "123456") {
-        return {
-            // Este JWT es falso, luego Spring Boot generará el real
-            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlVzdWFyaW8gV0FQRVIiLCJpYXQiOjE1MTYyMzkwMjJ9.MockSignature123",
-            user: {
-                id: 1,
-                name: "Administrador",
-                role: "STUDENT",
-            }
-        };
+        if (email === "admin@waper.app" && password === "123456") {
+            return {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlVzdWFyaW8gV0FQRVIiLCJpYXQiOjE1MTYyMzkwMjJ9.MockSignature123",
+                user: { id: 1, name: "Administrador", role: "STUDENT" }
+            };
+        }
+        throw new Error("Credenciales inválidas (Mock)");
     }
 
-    throw new Error("Credenciales inválidas");
+    // === LA CONEXIÓN REAL cuando provenga el token del backend ===
+    console.log("🟢 Conectando con Spring Boot real...");
+    const backendUrl = process.env.BACKEND_URL;
+    console.log("URL LEÍDA DEL .ENV:", backendUrl); // <-- Agrega esto
+    const response = await fetch(`${backendUrl}/api/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+
+    if (!response.ok) {
+        throw new Error("Credenciales inválidas en el servidor central");
+    }
+
+    return await response.json(); 
 };
