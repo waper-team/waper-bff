d646beca3439241f622fb5133fe9db4f230b9e0f
Said Quioto <said.quioto@uap.edu.ar>
2026-06-07 12:50:04 -0300

    feat(auth): actualizar logout para revocación atómica de tokens y aplicar patrón cascada en rutas

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { login } from '../controllers/auth.controller.js';
-import { testProtectedResource } from '../controllers/auth.controller.js';
+import { login, logout, testProtectedResource } from '../controllers/auth.controller.js';
+import { requireAuth } from '../middlewares/auth.middleware.js'; // Middleware para proteger rutas que requieren autenticación
 
 const router = Router();
 
 // Endpoint: POST http://localhost:3000/api/auth/login
 router.post('/login', login);
-router.get('/protected', testProtectedResource);// Endpoint de prueba para verificar que el BFF puede leer la cookie correctamente. eliminarlo cuando se integre con Spring Boot
+
+router.use(requireAuth); // Middleware que se aplicará a todas las rutas definidas después de esta línea para abajo
+// Rutas PROTEGIDAS (Inyectamos al guardia justo en el medio)
+router.post('/logout', logout);
+router.get('/protected', testProtectedResource);
+
+//router.post('/logout', requireAuth, logout);
+//router.get('/perfil-prueba', requireAuth, testProtectedResource);
 
 export default router;
